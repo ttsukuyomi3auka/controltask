c556b54e5e518a8d139f6eb706269e172f87845f
tsukuyomi <tsumi@mail>
2023-09-30 11:41:10 +0500

    newss

--- bin/controltask.dart
+++ bin/controltask.dart
@@ -1,16 +1,15 @@
 import 'package:controltask/controltask.dart' as controltask;
 import 'package:dio/dio.dart';
 
-void main(List<String> arguments) async{
+void main(List<String> arguments) async {
   Dio client = Dio();
   dynamic response = await client.get("https://dummyjson.com/products");
-  Map<String,dynamic> data = response.data;
+  Map<String, dynamic> data = response.data;
   List<dynamic> products = data["products"];
-  List<int> stocks=[];
-  for (dynamic s in products)
-  {
-      int stock = s['stock'];
-      stocks.add(stock);
+  List<int> stocks = [];
+  for (dynamic s in products) {
+    int stock = s['stock'];
+    stocks.add(stock);
   }
-  print(stocks.reduce((a, b) => a+b));
+  print(stocks.reduce((a, b) => a + b));
 }
